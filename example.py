64f47717595524144a4824afa17d8b3982bf2382
Saibhaveesh Beemireddy <beemires@flip1.engr.oregonstate.edu>
2021-06-06 21:21:13 -0700

    added tests and methods for multiple and divsion

--- example.py
+++ example.py
@@ -5,3 +5,11 @@ def add(a, b):
 
 def subtract(a, b):
     return a-b
+
+
+def division(a, b):
+    return a/b
+
+
+def multiply(a, b):
+    return a*b
--- test.py
+++ test.py
@@ -9,6 +9,12 @@ class TestCase(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(example.subtract(1, 1), 0)
 
+    def test_division(self):
+        self.assertEqual(example.division(5, 1), 5)
+
+    def test_multiply(self):
+        self.assertEqual(example.multiply(5, 1), 5)
+
 
 if __name__ == '__main__':
     unittest.main()
