ff67db99c30c9b3ca14366a15ec0d443f3c61dd6
Saibhaveesh Beemireddy <beemires@flip1.engr.oregonstate.edu>
2021-06-06 21:12:25 -0700

    added subtract method

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import example
 class TestCase(unittest.TestCase):
     def test_add(self):
         self.assertEqual(example.add(1, 2), 3)
-    
+
     def test_subtract(self):
         self.assertEqual(example.subtract(1, 1), 0)
 
